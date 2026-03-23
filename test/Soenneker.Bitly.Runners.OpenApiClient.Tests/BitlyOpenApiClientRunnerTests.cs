de4ba4983591ab4d5b356a1216d598518db2093d
Jake Soenneker <jake@soenneker.com>
2026-03-22 19:40:09 -0500

    Fixes build

--- test/Soenneker.Bitly.Runners.OpenApiClient.Tests/BitlyOpenApiClientRunnerTests.cs
+++ test/Soenneker.Bitly.Runners.OpenApiClient.Tests/BitlyOpenApiClientRunnerTests.cs
@@ -1,4 +1,3 @@
-using Soenneker.Bitly.Runners.OpenApiClient.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
 
@@ -7,11 +6,8 @@ namespace Soenneker.Bitly.Runners.OpenApiClient.Tests;
 [Collection("Collection")]
 public sealed class BitlyOpenApiClientRunnerTests : FixturedUnitTest
 {
-    private readonly IBitlyOpenApiClientRunner _openapirunner;
-
     public BitlyOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
-        _openapirunner = Resolve<IBitlyOpenApiClientRunner>(true);
     }
 
     [Fact]
