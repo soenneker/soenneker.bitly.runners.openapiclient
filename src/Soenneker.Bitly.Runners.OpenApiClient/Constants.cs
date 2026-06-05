ad336de002029714a321b67c642315e0ad3ba65f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:35:56 -0500

    xml commenting

--- src/Soenneker.Bitly.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.Bitly.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.Bitly.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Bitly.OpenApiClient";
 }
